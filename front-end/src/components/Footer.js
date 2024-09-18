5f50e6cff445f52fa474c0544475e5be5a8a75c0
RaniaElouni <raniaelouni421@gmail.com>
2024-09-18 20:22:52 +0100

    footer

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -2,14 +2,14 @@ import React from "react";
 import email from "../assets/images/email.png";
 import tel from "../assets/images/tel.png";
 import heart from "../assets/images/heart.png";
-import logo from "../assets/images/logo.jpg";
+import logo from "../assets/images/logojalyss.jpg";
 
 function Footer() {
   return (
-    <div className=" my-5">
+    <div className="mT-5">
       <div
         className="text-center text-lg-start text-white"
-        style={{ backgroundColor: "#1a2e45" }}
+        style={{ backgroundColor: "#47174B" }}
       >
         <div
           className=" d-flex flex-wrap justify-content-around gap-5 p-5"
@@ -24,8 +24,7 @@ function Footer() {
               style={{ objectFit: "cover", borderRadius: "50%" }}
             />
             <p className="mt-2 text-center">
-              Union Générale Tunisienne des Etudiants (UGTE) - <br /> الإتحاد
-              العام التونسي للطلبة
+            رياديون في عالم المعرفة
             </p>
           </div>
 
@@ -38,7 +37,7 @@ function Footer() {
               style={{ objectFit: "cover" }}
             />
             <p className="mt-2" style={{ direction: "ltr" }}>
-              (+216) 50 031 190
+              (+216) 003 165 51
             </p>
           </div>
           <div className="d-flex flex-column justify-content-center align-items-center">
@@ -49,7 +48,7 @@ function Footer() {
               width={60}
               style={{ objectFit: "cover" }}
             />
-            <p className="mt-2">ugte.tn@hotmail.com</p>
+            <p className="mt-2">jalysscom.book@gmail.com</p>
           </div>
         </div>
 
@@ -58,23 +57,10 @@ function Footer() {
           style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
         >
           <div>
-            <span>© 2023 </span>All rights reserved. | This template is made
+            <span>© 2024 </span>All rights reserved. | This template is made
             with
-            <img
-              src={heart}
-              alt="heart"
-              width={20}
-              height={20}
-              style={{ marginRight: 6, marginLeft: 6 }}
-            />
-            by
-            <a
-              className="text-white"
-              href="https://www.sfectoria.com"
-              style={{ marginLeft: 10 }}
-            >
-              SFECTORIA
-            </a>
+          TUNIRISE
+           
           </div>
         </div>
       </div>
--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -17,49 +17,8 @@ function Profile() {
   }, [memberId]);
   return (
     <>
-      <div className="d-flex justify-content-center p-sm-5 p-3">
-        <div className="col-sm-9 col col-md-6 position-relative">
-          <img src={memberCard} alt="memberCard" className="w-100" />
-          <img
-            src={member?.avatarUrl}
-            className="position-absolute w-25 rounded-circle"
-            style={{ right: "11%", top: "30%" }}
-          />
-          <span
-            className="position-absolute w-25 fontCard "
-            style={{ left: "10%", top: "39%" }}
-          >
-            {member?.firstNameAr + " " + member?.lastNameAr}
-          </span>
-          <span
-            className="position-absolute w-25 fontCard "
-            style={{ left: "10%", top: "46%" }}
-          >
-            {member?.cin}
-          </span>
-          <span
-            className="position-absolute w-25 fontCard "
-            style={{ left: "10%", top: "53%" }}
-          >
-            {member?.dob?.slice(0, 10)}
-          </span>
-          <span
-            className="position-absolute fontCard "
-            style={{ left: "5%", top: "60%", width: "30%", lineHeight: 1 }}
-          >
-            {member?.UniversityPart?.nameAr}
-          </span>
-          <span
-            className="position-absolute fontCard w-25 "
-            style={{ left: "10%", top: "70%", width: "30%", lineHeight: 1 }}
-          >
-            {member?.Grade?.nameAr}
-          </span>
-        </div>
-      </div>
-      <p className="text-center p-3" style={{color:'red'}} >
-        تنويه : هذه البطاقة الرقمية ذات صلوحية مؤقتة لغاية استلام البطاقة
-        الرسمية
+      <p className="text-center pb-5" >
+       تهانينا! لقد اكتمل التسجيل بنجاح. شكرًا لانضمامك!
       </p>
     </>
   );
